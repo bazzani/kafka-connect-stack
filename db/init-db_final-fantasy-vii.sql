c8e6ec86c25906640e559dcf46635e82e86660dd
Barry Evans <barryevans80@gmail.com>
2024-04-25 22:56:45 +0900

    Rename table/Entity to use singular form, update DLQ name
    It is cleaner to name the DB table in singular form as this will also
    map to the upcoming JPA `@Entity` class neater too - `final_fantasy_vii_ally_update`.
    
    Some fields in the AVRO schema/data have been updated to use pascalCase,
    not snake_case, and the namespace changed to `com.bevans.avro.ffvii`.
    
    The Connector configuration has also been updated to reflect the new
    table name and AVRO field names, and the DLQ in the connector has been
    changed to have a `.DLT` suffix to align with the recommended name from
    Spring Docs - `ff_vii.ally_updates.v1.DLT`
    - https://docs.spring.io/spring-kafka/reference/kafka/annotation-error-handling.html#dead-letters

--- db/init-db_final-fantasy-vii.sql
+++ db/init-db_final-fantasy-vii.sql
@@ -1,5 +1,5 @@
 -- table used to store live updates of each ally as you play the game
-create table if not exists final_fantasy_vii_allies
+create table if not exists final_fantasy_vii_ally_update
 (
     name           text      not null,
     update_ts      timestamp not null,
@@ -11,9 +11,9 @@ create table if not exists final_fantasy_vii_allies
     ps_store_cost  float8,
     weapon         jsonb     not null,
     materia        jsonb,
-    constraint final_fantasy_vii_allies_pk
+    constraint final_fantasy_vii_ally_update_pk
         primary key (name, update_ts)
 );
 
-alter table final_fantasy_vii_allies
+alter table final_fantasy_vii_ally_update
     owner to kafka_connect_user;
