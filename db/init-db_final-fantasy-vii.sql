29e10325260c523bc9b76a750f783301df4180b5
Barry Evans <barryevans80@gmail.com>
2024-04-29 13:59:30 +0900

    Add postgres service - split init and seed data scripts

--- db/init-db_final-fantasy-vii.sql
+++ db/init-db_final-fantasy-vii.sql
@@ -17,16 +17,3 @@ create table if not exists final_fantasy_vii_allies
 
 alter table final_fantasy_vii_allies
     owner to kafka_connect_user;
-
---
--- Add seed data for Cloud, the main character in the story
-insert into final_fantasy_vii_allies (name, update_ts, age, exp, hp, mp, win_loss_ratio, ps_store_cost, weapon, materia)
-values ('Cloud Strife', '2024-04-05 22:35:00.000000', 30, 32000, 5999, 128, 12.12345, 123.1234567, '{
-  "name": "Buster Sword",
-  "attack": 22,
-  "magic_attack": 22
-}', '[
-  "Healing",
-  "Fire",
-  "Raise"
-]');
