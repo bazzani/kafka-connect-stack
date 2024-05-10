782c32d52b12b6a43d846bd724e43d90d20b3e48
Barry Evans <bazzani@users.noreply.github.com>
2024-05-10 20:22:56 +0900

    Rename DB table to use singular form, update DLQ name (#5)
    It is cleaner to name the DB table in singular form as this will also
    map to the upcoming JPA `@Entity` class neater too
     - `final_fantasy_vii_ally_update`.
    
    Some fields in the AVRO schema/data have been updated to use pascalCase,
    not snake_case, and the namespace changed to `com.bevans.avro.ffvii`.
    
    The Connector configuration has also been updated to reflect the new
    table name and AVRO field names, and the DLQ in the connector has been
    changed to have a `.DLT` suffix to align with the recommended name from
    Spring Docs - `ff_vii.ally_updates.v1.DLT`
     - https://docs.spring.io/spring-kafka/reference/kafka/annotation-error-handling.html#dead-letters

--- db/seed-db_final-fantasy-vii.sql
+++ db/seed-db_final-fantasy-vii.sql
@@ -1,6 +1,7 @@
 --
 -- Add seed data for Cloud, the main character in the story
-insert into final_fantasy_vii_allies (name, update_ts, age, exp, hp, mp, win_loss_ratio, ps_store_cost, weapon, materia)
+insert into final_fantasy_vii_ally_update
+(name, update_ts, age, exp, hp, mp, win_loss_ratio, ps_store_cost, weapon, materia)
 values ('Cloud Strife', '2024-04-05 22:35:00.000000', 30, 32000, 5999, 128, 12.12345, 123.1234567, '{
   "name": "Buster Sword",
   "attack": 22,
