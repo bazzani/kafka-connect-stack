916908f1c860bd6f883494f0a2a7dc55215cf2c2
Barry Evans <barryevans80@gmail.com>
2024-05-26 21:39:56 +0900

    dont disable SMT test as JaCoCo will fail

--- connect-smt-lib/src/test/java/com/bevans/kafka/connect/transforms/StructToJsonTransformTest.java
+++ connect-smt-lib/src/test/java/com/bevans/kafka/connect/transforms/StructToJsonTransformTest.java
@@ -7,7 +7,6 @@ import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -34,7 +33,6 @@ class StructToJsonTransformTest {
     }
 
     @Test
-    @Disabled("i just hate coverage on code 🙈")
     void shouldApplyAndTransformToJsonObject() {
         // given
         var weaponSchema = SchemaBuilder.struct()
